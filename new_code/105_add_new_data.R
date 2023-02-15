ca3b671b219eee2f62bd11424f71a51ab123933b
jimrothstein <jimrothstein@gmail.com>
2023-02-14 16:20:07 -0800

    added format (columns) and new columns to health data

--- new_code/105_add_new_data.R
+++ new_code/105_add_new_data.R
@@ -4,21 +4,48 @@
 ####	lab_data:  Manually Add new lab data SINCE last google entry.
 library(data.table)
 
+
 #	add like tribble (this is fake)
-dt_add  <- data.table::fread("Date, Lab_Test, Lab_Result
-9/01/22, Tot_Cholesterol, 131
-9/01/22, Trig, 73
+dt_add  <- data.table::fread("Date, Lab_Test, Lab_Result, Lab, Comment
+9/01/22, Tot_Cholesterol, 131,
+9/01/22, Trig, 73,
 9/01/22, HDL, 52
 9/01/22, LDL, 64
-9/01/22, Uric_Acid (blood), 6.0 mg/dL
-9/02/22, UrOx, 100
-9/02/22, UrOx, 90
-9/03/22, UrOx, 50
-")
-dt_add
+9/01/22, Serum_Uric, 6.0, Kaiser,
+9/01/22, Ur_Ca24, 217
+9/01/22, Ur_Citrate24, 749
+9/01/22, Ur_Vol24, 6.2
+9/01/22, Ur_pH, 7.7
+9/01/22, UrOx, 55
+9/01/22, Ur_Uric24, 0.422
+9/01/22, Serum_Ca, 9.4
+9/01/22, Serum_P, 3.8
+9/01/22, Serum_Cl, 99
+9/01/22, Serum_Cr, 1.22 
+9/01/22, Serum_Uric, 6.1, Litholink
+9/01/22, eGFR, 65 
+5/12/22, Ur_Pr/Cr, 0.91
+5/12/22, TSH, 1.35
+5/12/22, PTH, 99
+5/12/22,  A1C, 5.5
+5/12/22,  GLU, 102
+5/12/22,  Serum_Cr, 1.12, Kaiser,
+5/12/22,  BUN, 26, Kaiser,
+5/12/22,  eGFR, 72, Kaiser, !
+5/12/22,  
+5/12/22,  
+5/12/22,  
+", fill=T)
 
 
-dt_add[, .(Date=as.Date(Date), Lab_Test, Lab_Result)]
+dt_add[, .(Date=sort(as.Date(Date)), format(Lab_Test, justify="left"), Lab_Result, Lab, Comment)]
+
+# -------------------------------------  TODO:  format Lab_Test, left align-----------------------------
+format_col.complex = function(x, ...) sprintf('(%.1f, %.1fi)', Re(x), Im(x))
+
+format_col = function(x, ...) sprintf('(%.s)', x)
+format_col(dt_add$Lab_Test)
+# ----------------------------------------------------------------------------
 
 #	To cut and paste
 dput(dt_add)
@@ -31,6 +58,7 @@ dput(dt_add)
 ##-------------------------------------
 
 ## practice with legacy; TODO:  run lookup against newly entered data
+## Unique Test_Names
 {
 	library(data.table)
 #	
@@ -39,6 +67,10 @@ dput(dt_add)
 	dt  <- as.data.table(read.csv(IN_FILE))
 	dt
 	dt[, .(Test_Name)]
+
+	dt[, .(sort(unique(Test_Name)))]
+
+
 }
 
 
